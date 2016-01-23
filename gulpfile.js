ff296938df3cb35e11f971b6a608078f6a6f5f7f
visermort <oxygenn@list.ru>
2016-01-23 14:54:00 +0300

    Сверстана боковая панель

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,24 @@
- var gulp=require("gulp"),  
-     browserSync=require('browser-sync');  
+ "use strict";
+
+ var gulp=require("gulp");  
+ var browserSync=require('browser-sync').create();  
                    
  gulp.task('server', function () {   
-   browserSync({  
+   browserSync.init({  
      port: 9000,  
      server: {  
        baseDir: 'app'  
      }  
-   });  
- });  
+   });  
+ })  
    
  gulp.task('watch', function () {  
    gulp.watch([  
      'app/*.html',  
      'app/js/**/*.js',  
      'app/css/**/*.css'  
-   ]).on('change', browserSync.reload);  
- });  
+   ]).on('change', browserSync.reload)  
+ })  
    
- gulp.task('default', ['server', 'watch']);    
+ gulp.task('default', ['server', 'watch'])    
 
